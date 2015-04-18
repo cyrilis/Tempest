2af4f805675d4942dcc85781eaeca0f4026c700d
Cyril Hou <houshoushuai@gmail.com>
2015-04-18 22:44:19 +0800

    Initial Commit

---
+++ Tempest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tempest
+//
+//  Created by Cyril Hou on 15/4/18.
+//  Copyright (c) 2015年 Cyrilis.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tempest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tempest
+//
+//  Created by Cyril Hou on 15/4/18.
+//  Copyright (c) 2015年 Cyrilis.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
